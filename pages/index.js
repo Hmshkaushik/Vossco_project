c13f7a9f617c298eb6784594d303cfcb5ea853d4
Hmshkaushik <himanshukaushik9873@gmail.com>
2025-08-03 00:10:10 +0530

    Ensure all files are tracked

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>✅ Deployed Successfully!</h1>
+      <p><a href="/city/bangalore">Go to Bangalore Listings</a></p>
+    </div>
+  );
+}
